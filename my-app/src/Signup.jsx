4f3500d7910033df46d7538a8ad04cfd1ec360d7
nikkanap <nenaputo@up.edu.ph>
2026-04-14 15:01:10 +0800

    Add Signup page and route

---
+++ my-app/src/Signup.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import { Link } from 'react-router-dom'
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <section class="login">
+        <h1>Create Your Account</h1>
+        <p>Join our community and help pets find loving homes</p>
+        <form>
+          <label>First Name <input type="text" placeholder="e.g. Juan"/></label>
+          <label>Last Name <input type="text" placeholder="e.g. dela Cruz"/></label>
+          <label>Email address <input type="text" placeholder="e.g. imarubberduck@duckdomain.xyz"/></label>
+          <label>Phone Number <input type="number" placeholder="09XXXXXXXXX"/></label>
+          
+          <label>Password <input type="password" placeholder="secureHashSHA12123"/></label>
+          <label>Confirm Password <input type="password" placeholder="secureHashSHA12123"/></label>
+          
+          <button>SIGN UP</button>
+        </form>
+        <Link to="/login">Already have an account? <strong>Sign in</strong></Link>
+      </section>
+
+      <Footer />
+    </> 
+  )
+}
+
+export default App
