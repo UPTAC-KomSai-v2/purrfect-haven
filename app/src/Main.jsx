38b14de07436fe4fc55156cd7e5f6ac0b1c2dd13
nikkanap <nenaputo@up.edu.ph>
2026-08-08 19:27:16 +0800

    Rename Main.jsx

--- app/src/Main.jsx
+++ app/src/Main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
-import App from './App'
+import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx';
 
 /* Modify for the Page layers example Home.jsx */
